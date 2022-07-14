84fd8f7aa3ecc418c9575104bee9a73ad654c054
Md Sharique Anwer <shariqueanwer002@gmail.com>
2022-07-15 04:49:07 +0530

    redux-toolkit update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
+  };
+  const addBy = () => {
+    dispatch(actions.addBy(10));
   };
 
   return (
@@ -17,6 +21,7 @@ function App() {
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
-import { legacy_createStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = { counter: 0 }, action) => {
-  if (action.type === "INC") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "DEC") {
-    return { counter: state.counter - 1 };
-  }
-  return state;
-};
-
-const store = legacy_createStore(reducerFn);
-
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    addBy(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
+export const actions = counterSlice.actions;
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 export default store;
+
+// import { legacy_createStore } from "@reduxjs/toolkit";
+
+// const reducerFn = (state = { counter: 0 }, action) => {
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   if (action.type === "ADD") {
+//     return { counter: state.counter + action.payload };
+//   }
+//   return state;
+// };
+
+// const store = legacy_createStore(reducerFn);
+
+// export default store;
